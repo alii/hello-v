4304679365a5ab8ec545d5282e8952a11bd59c23
Alistair Smith <alistairsmith01@gmail.com>
2021-05-13 12:15:20 +0100

    fix: nillable left/right nodes

--- structs/index.v
+++ structs/index.v
@@ -4,10 +4,10 @@ pub struct Node<T> {
 	pub:
 		val T
 	pub mut:
-		left  &Node<T> = &Node(0)
-		right &Node<T> = &Node(0)
+		left  &Node<T> = 0
+		right &Node<T> = 0
 }
 
-pub fn (t &Node) exists() bool {
-	return t != &Node(0)
+pub fn (t &Node<T>) exists<T>() bool {
+	return !isnil(t)
 }
